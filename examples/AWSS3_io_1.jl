c15dbf5dbb66ce0af0977f6af8aa4c4360c7cc44
Henryk Modzelewski <henryk_modzelewski@mac.com>
2018-03-26 17:12:41 -0700

    some examples

---
+++ examples/AWSS3_io_1.jl
@@ -0,0 +1,18 @@
+# AWS S3 example
+# to put array into a bucket 'slimbucket' under name 'tmp/test/small' 
+
+# safe for multiple Cloud providers mixed in the same code
+
+using AWSCore
+using CloudExtras
+
+aws = aws_config(region="us-west-2")
+
+a=randn(11,12,13);
+
+AWSextras.array_put(aws, "slimbucket", "tmp/test/small", a);
+
+A=AWSextras.array_get(aws, "slimbucket", "tmp/test/small");
+
+AWSextras.array_delete(aws, "slimbucket", "tmp/test/small");
+
---
+++ examples/AWSS3_io_2.jl
@@ -0,0 +1,18 @@
+# AWS S3 example
+# to put array into a bucket 'slimbucket' under name 'tmp/test/small' 
+
+# safe only if using one Cloud provider
+
+using AWSCore
+using CloudExtras.AWSextras
+
+aws = aws_config(region="us-west-2")
+
+a=randn(11,12,13);
+
+array_put(aws, "slimbucket", "tmp/test/small", a);
+
+A=array_get(aws, "slimbucket", "tmp/test/small");
+
+array_delete(aws, "slimbucket", "tmp/test/small");
+
