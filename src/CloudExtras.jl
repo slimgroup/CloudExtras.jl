c9bab58f5c192ce09ff6d987bb67e3ea2ffe615f
Henryk Modzelewski <henryk_modzelewski@mac.com>
2018-09-10 16:29:23 -0700

    any_* GCP bucket routines

--- src/CloudExtras.jl
+++ src/CloudExtras.jl
@@ -3,5 +3,7 @@ module CloudExtras
 include("common.jl")
 include("AWSextras.jl")
 export AWSextras
+include("GCPextras.jl")
+export GCPextras
 
 end # module
---
+++ src/GCPextras.jl
@@ -0,0 +1,8 @@
+module GCPextras
+    using GoogleCloud
+    using Blosc
+    using ..common: file_parts
+
+    include("GCPextras/any.jl")
+
+end
